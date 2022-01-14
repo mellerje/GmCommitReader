966e2067966aca9fd3eb45715c91108299f5b5bf
mellerje <mellerje@gmail.com>
2022-01-13 23:37:34 -0500

    Design Start
    Added initial design for the application.
    Added refresh button
    Added refreshed toast message
    Added list view to show the retrieved items.

--- app/src/main/java/com/meller/gmcommitreader/MainActivity.java
+++ app/src/main/java/com/meller/gmcommitreader/MainActivity.java
@@ -2,13 +2,45 @@ package com.meller.gmcommitreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button refreshButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        refreshButton = (Button) findViewById(R.id.refreshButton);
+        refreshButton.setOnClickListener(this);
+    }
+
+    private boolean connectToRepository()
+    {
+        return true;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        refreshRepository();
+    }
+
+    private void refreshRepository() {
+        showToast("Repository Refreshed");
+    }
+
+    private void showToast(String toastMessage) {
+        Context context = getApplicationContext();
+        CharSequence text = toastMessage;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
